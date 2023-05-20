017d7e31ba253c8c141f2b8af7296246c702f2ae
nakahara-c <ttcocoan@gmail.com>
2023-05-21 02:06:53 +0900

    initial commit

---
+++ content.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+const NUMBER1_USER = 'cocoa';
+
+document.querySelector("body > center > table:nth-child(13) > tbody > tr > td > table > tbody > tr:nth-child(28) > td:nth-child(2) > b > a");
